3fc683efe15a6e546a1dc9e7c598430220230184
Anselme Irumva <andesanselme@gmail.com>
2026-06-25 20:13:30 +0200

    Initialize server with Express and add package.json for dependencies

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("", (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
